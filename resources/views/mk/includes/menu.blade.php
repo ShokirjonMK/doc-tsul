750b64069db85ebe6688ed8bed263c22919c0571
ShokirjonMK <57320165+ShokirjonMK@users.noreply.github.com>
2021-05-19 08:47:42 +0500

    new rolwe

--- resources/views/mk/includes/menu.blade.php
+++ resources/views/mk/includes/menu.blade.php
@@ -1,11 +1,11 @@
-@if(Auth::user()->role == 777)
+@if(Auth::user()->role == 777 || Auth::user()->role == 333)
 {{--	<li class="dropdown @if( ( Request::is('backoffice/strc') ) ||  ( Request::is('backoffice/department') ) ) show @endif">--}}
-	<li class="show">
+	<li class="@if(  Request::is('mk/doc')  ) show @endif">
 		<a href="{{ route('doc.index')}}" class="dropdown-toggle">
 			<span class="micon dw dw-house-1"></span><span class="mtext">Asosiy </span>
 		</a>
 	</li>
-	<li class=" ">
+	<li class="@if(  Request::is('mk/user')  ) show @endif">
 		<a href="{{ route('user.index')}}" class="dropdown-toggle">
 			<span class="micon dw dw-user"></span><span class="mtext">Foydalanuvchilar </span>
 		</a>
